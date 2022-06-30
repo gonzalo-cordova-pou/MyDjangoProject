94c0006b22100b5e8f427e1cd6a805909cbb417f
gonzalo-cordova-pou <gcp@vincesconsulting.com>
2022-06-30 11:03:44 +0200

    Progress

--- devfinder/projects/views.py
+++ devfinder/projects/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
-# Create your views here.
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projectsList = Project.objects.all()
+    context = {
+        "projects":projectsList
+        }
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single_project.html')
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    return render(request, 'projects/single_project.html',{
+        'project':projectObj,
+        'tags':tags
+    })
+
+def createProject(request):
+    context = {'form':ProjectForm}
+    return render(request, "projects/project_form.html", context)
