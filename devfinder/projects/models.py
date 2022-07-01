334a393ae9ee855edc27700d0349c8912665a5f7
gonzalo-cordova-pou <gcp@vincesconsulting.com>
2022-07-01 19:23:22 +0200

    Static files: image, style..

--- devfinder/projects/models.py
+++ devfinder/projects/models.py
@@ -1,3 +1,5 @@
+from contextlib import nullcontext
+from email.policy import default
 from django.db import models
 import uuid
 
@@ -6,6 +8,7 @@ class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
     title = models.CharField(max_length=255)
+    featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
